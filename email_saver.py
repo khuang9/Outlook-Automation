9c570dbc34e59e5c92c7a02d70c93a4aa5f8e3a4
khuangfirst <khuangfirst@gmail.com>
2024-07-24 15:40:06 -0400

    Add more sound alerts and messages

--- email_saver.py
+++ email_saver.py
@@ -85,3 +85,14 @@ if recip.Resolved:
     print(f"Filtered out {summary[1]} forwarded messages")
     print(f"Filtered out {summary[2]} duplicate messages")
     print(f"Skipped {summary[3]} messages")
+    
+    freqs = {"C3":131, "C#3":139, "D3":147, "D#3":156, "E3":165, "F3":175, "F#3":185, "G3":196, "G#3":208, "A3":220, "A#3":233, "B3":247, 
+             "C4":262, "C#4":277, "D4":294, "D#4":311, "E4":330, "F4":349, "F#4":370, "G4":392, "G#4":415, "A4":440, "A#4":466, "B4":494, 
+             "C5":523, "C#5":554, "D5":587, "D#5":622, "E5":659, "F5":698, "F#5":740, "G5":784, "G#5":831, "A5":880, "A#5":932, "B5":988, }
+  
+    song = ["C4", "D4", "C4", "A3", "A3", "A3", "G3", "A3", "A#3", "A3"]
+    for note in song:
+        winsound.Beep(freqs[note], 500)
+        
+else:
+    print("Recipient could not be resolved")
