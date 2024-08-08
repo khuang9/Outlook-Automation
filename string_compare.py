1cf4f42e0caeb279b7a50a283b24ae74173a7263
khuangfirst <khuangfirst@gmail.com>
2024-08-08 00:27:07 -0400

    Allow for stripping of punctuation and numbers when comparing strings

--- string_compare.py
+++ string_compare.py
@@ -20,11 +20,32 @@ def strip_dupes(s):
 def strip_spaces(s):
     return s.replace(" ", "")
 
+def strip_punc(s):
+    punctuation = ["-", "_", ".", ","]
+   
+    for p in punctuation:
+        s = s.replace(p, "")
+   
+    return s
 
-def process_words(orig, compare):
+def strip_num(s):
+    for n in range(48, 58):
+        s = s.replace(chr(n), "")
+   
+    return s
+
+def process_words(orig, compare, strip_puncs, strip_nums):
     removed = 0
     orig = strip_dupes(strip_spaces(orig))
     compare = strip_dupes(strip_spaces(compare))
+   
+    if strip_puncs:
+        orig = strip_punc(orig)
+        compare = strip_punc(compare)
+       
+    if strip_nums:
+        orig = strip_num(orig)
+        compare = strip_num(compare)
    
     # might remove from longer one instead of compare (maybe)
     i = 0
@@ -45,18 +66,20 @@ def similarity(orig, compare):
     for i in range(min_len):
         if orig[i] == compare[i]:
             matches += 1
+           
+    if min_len == 0:
+        return 0
+    else:
+        return matches/min_len
    
-    return matches/min_len
-   
-def match_degree(orig, compare):
+def match_degree(orig, compare, strip_puncs=False, strip_nums=False):
     if orig != compare:
         penalty = -0.01
     else:
         penalty = 0
        
-    orig, compare, removed = process_words(orig, compare)
+    orig, compare, removed = process_words(orig, compare, strip_puncs, strip_nums)
     sim = similarity(orig, compare)
     rem = 1 - removed/(removed + len(compare))
    
     return (sim + rem)/2 + penalty
-  
